2e9e633074c24e6fe717690585cd18d8146d00e6
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-07 16:29:39 +0100

    Update emzyCash.py

--- emzyCash.py
+++ emzyCash.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 
 #image = Image.open("profile_pic.jpg")
 #st.image(image, caption='Think EmzyCash, Think Easy Cash')
-st.image("profile_pic.jpg", caption='Think Easy Cash, Think EmzyCash', width=150)
+st.image("profile_pic.jpg", caption='Think Easy Cash, Think EmzyCash', width=1000)
 # Caption
 st.write("""
 # EmzyCash:
