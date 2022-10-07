bd57366ef8e6caf11bf040585be44f886a2a0386
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-07 16:03:48 +0100

    Update emzyCash.py

--- emzyCash.py
+++ emzyCash.py
@@ -8,9 +8,9 @@ from PIL import Image
 
 
 
-#image = Image.open(https://github.com/ClassicCollins/classic_projects/blob/Python_projects/profile_pic.jpg "Data scientist/Geoscientist")
-#st.image(image, caption='Think EmzyCash, Think Easy Cash')
-st.image(https://github.com/ClassicCollins/classic_projects/blob/Python_projects/profile_pic.jpg)
+image = Image.open("https://github.com/ClassicCollins/classic_projects/blob/Python_projects/profile_pic.jpg")
+st.image(image, caption='Think EmzyCash, Think Easy Cash')
+#st.image("https://github.com/ClassicCollins/classic_projects/blob/Python_projects/profile_pic.jpg")
 # Caption
 st.write("""
 # EmzyCash:
