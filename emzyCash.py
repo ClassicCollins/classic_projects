d36b8b543f232605c3769e008ace54f03da1c5d0
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-07 14:45:44 +0100

    Update emzyCash.py

--- emzyCash.py
+++ emzyCash.py
@@ -7,6 +7,8 @@ from matplotlib import pyplot as plt
 
 st.write("""
 # EmzyCash
+Inline-style: 
+![alt text]( "Data scientist/Geoscientist")
 This is an investment platform that fascinates your interest on investment on financial asset such as stocks, bonds, 
 options, forex & cryptos. This app is designed basically for stock investment.
 
