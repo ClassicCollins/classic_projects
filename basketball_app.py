49e8593df9acc18e31f3a1ffd28b3fd03ac0636a
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-19 15:45:55 +0100

    Update basketball_app.py

--- basketball_app.py
+++ basketball_app.py
@@ -9,8 +9,10 @@ st.title('NBA Player Stats Explorer')
 
 st.markdown("""
 This app performs simple webscraping of NBA player stats data!
+* **NBA PLayers Statistics:** gives you stats of NBA Players from 1950 - 2022
 * **Python libraries:** base64, pandas, streamlit
 * **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+* **More Info:** Contact ugwuozorcollinsemezie@gmail.com 
 """)
 
 st.sidebar.header('User Input Features')
